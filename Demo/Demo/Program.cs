94ab6dd6179003f344bb3210b2b47837826a589a
Sandeep <sandeepmailarkar093@gmail.com>
2023-05-17 17:34:58 +0530

    Todays Program

---
+++ Demo/Demo/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal class Program
+       
+    {
+        static void Main(string[] args)
+        {
+            //int a = 1;
+            // float b = 2.3F;
+            //double c = 5.6;
+            //bool d = true;
+            //char e = 'S';
+            //string f = "Hello world";
+            //Console.WriteLine(a);
+            //Console.WriteLine(b);
+            //Console.WriteLine(c);
+            //Console.WriteLine(d);
+            //Console.WriteLine(e);
+            //Console.WriteLine(f);
+            // Console.ReadLine(); 
+
+
+
+
+
+
+
+
+            //Console.WriteLine("Enter Your Age here:");
+            //string agestr = Console.ReadLine();
+            //int age = Convert.ToInt32(agestr);
+            //if (age > 18)
+            //{
+
+            //    Console.WriteLine("You Can Drive...");
+
+            //}
+
+            //else if(age > 10) 
+            //{
+
+
+            //    Console.WriteLine("You are bellow 10");
+            //}
+
+            //else if (age < 12)
+            //{
+
+
+            //    Console.WriteLine("Go to Higher School");
+            //}
+
+            //else   
+
+            //{
+            //    Console.WriteLine("you Are Not 18 Years");
+
+            //}
+
+
+
+            //    Console.ReadLine();
+
+
+
+
+
+
+
+
+
+            //int i = 0;
+            //while (i < 10)
+
+            //{
+            //    Console.WriteLine(i + 1);
+            //    i++;
+            //}
+            //Console.ReadLine();
+
+
+            //int i = 0;
+
+
+            //do  {
+            //    Console.WriteLine(i + 1);
+            //    i++;
+            //} while (i < 10);
+
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
